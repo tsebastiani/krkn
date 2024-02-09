1e32002cfc56c02c705f182cb1c9e8aa2f78ff42
Tullio Sebastiani <tsebasti@redhat.com>
2024-02-09 14:54:19 +0100

    final functional tests
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- CI/tests/test_telemetry.sh
+++ CI/tests/test_telemetry.sh
@@ -7,8 +7,11 @@ trap finish EXIT
 
 
 function functional_test_telemetry {
+  AWS_CLI=`which aws`
+  [ -z "$AWS_CLI" ]&& echo "AWS cli not found in path" && exit 1
+  [ -z "$AWS_BUCKET" ] && echo "AWS bucket not set in environment" && exit 1
+
   export RUN_TAG="funtest-telemetry"
-  #yq -i '.input_list[0].node_selector={"kubernetes.io/hostname":"kind-worker2"}' scenarios/arcaflow/cpu-hog/input.yaml
   yq -i '.telemetry.enabled=True' CI/config/common_test_config.yaml
   yq -i '.telemetry.full_prometheus_backup=True' CI/config/common_test_config.yaml
   yq -i '.telemetry.run_tag=env(RUN_TAG)' CI/config/common_test_config.yaml
@@ -17,6 +20,12 @@ function functional_test_telemetry {
   export post_config=""
   envsubst < CI/config/common_test_config.yaml > CI/config/telemetry.yaml
   python3 -m coverage run -a run_kraken.py -c CI/config/telemetry.yaml
+  RUN_FOLDER=`cat CI/out/test_telemetry.out | grep amazonaws.com | sed -rn "s#.*https:\/\/.*\/download/(.*)#\1#p"`
+  $AWS s3 ls "s3://$AWS_BUCKET/$RUN_FOLDER/" | awk '{ print $4 }' > remote_files
+  cat remote_files | grep events-00.json || ( echo "FAILED: events-00.json not uploaded" && exit 1 )
+  cat remote_files | grep prometheus-00.tar || ( echo "FAILED: prometheus backup not uploaded" && exit 1 )
+  cat remote_files | grep telemetry.json || ( echo "FAILED: telemetry.json not uploaded" && exit 1 )
+
   echo "Telemetry Collection: Success"
 }
 
